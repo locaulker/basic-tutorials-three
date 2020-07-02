a499f724f0ad6714bc1182f4403a7a21d01d7448
Lawrence <locaulker@gmail.com>
2020-07-02 06:58:19 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
+import PropTypes from "prop-types"
 
-function App() {
+// 1. Install prop-types as aseparate module
+// 2. Built-in type checking to validate props
+// 3. Prop-types include: array, bool, func, object, string, number
+
+const Person = ({ img, name, age, info }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <article>
+      <img src={img} alt="Person" />
+      <h4>Name:{name}</h4>
+      <h4>Age:{age}</h4>
+      <h4>Info:{info}</h4>
+    </article>
+  )
 }
 
-export default App;
+Person.propTypes = {
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired
+}
+
+Person.defaultProps = {
+  img: "https://randomuser.me/api/portraits/thumb/men/45.jpg",
+  name: "James Doe",
+  age: 34,
+  info: "Defaullt info about a person"
+}
+
+class PersonList extends Component {
+  state = {
+    people: [
+      {
+        id: 1,
+        img: "https://randomuser.me/api/portraits/thumb/men/65.jpg",
+        name: "John",
+        age: 24
+      },
+      {
+        id: 2,
+        // img: "https://randomuser.me/api/portraits/thumb/men/34.jpg",
+        // name: "Bob",
+        age: 27
+      },
+      {
+        id: 3,
+        img: "https://randomuser.me/api/portraits/thumb/men/87.jpg",
+        name: "Peter",
+        age: 27,
+        info: "A brief bio about Peter"
+      }
+    ]
+  }
+
+  render() {
+    return (
+      <section>
+        {this.state.people.map(person => (
+          <Person
+            key={person.id}
+            img={person.img}
+            name={person.name}
+            age={person.age}
+            info={person.info}
+          />
+        ))}
+      </section>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return <PersonList />
+  }
+}
+
+export default App
